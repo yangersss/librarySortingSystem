1b54159b7ca9eb7a1ea29deb32cd8fd352f6a55c
yangersss <edwardyang609@gmail.com>
2023-01-13 12:08:46 -0500

    feelsGoodMan big progress

--- Book.java
+++ Book.java
@@ -1,15 +1,23 @@
-import java.nio.channels.Pipe;
-
 public abstract class Book{
-    String title;
-    String genre;
-    String author;
-    String subject;
-    int edition;
-    int qty;
+    public String title;
+    public String genre;
+    public String author;
+    public String subject;
+    public int edition;
+    public int qty;
+
+    //longer projects should use private attributes and setter/getter methods
 
     public void book(String str){
-        
+        // title, genre, author, subject, edition, qty
+
+        String[] words = str.split(",");
+        title = words[0];
+        genre = words[1];
+        author = words[2];
+        subject = words[3];
+        edition = Integer.parseInt(words[4]);
+        qty = Integer.parseInt(words[5]);
     }
 
     public void printInfo(){
--- BookList.java
+++ BookList.java
@@ -1,7 +1,35 @@
+import java.util.ArrayList;
+
 public class BookList {
     ArrayList<Book> books;
 
-    public void addToList(Book book) {
+    public void addToList(Book book){
+        books.add(book);
+    }
+
+    public void removeFromList(String author, String title){
+        for (Book book : books){
+            if (book.author.equals(author) && book.title.equals(title)){
+                books.remove(book);
+                System.out.println("Alright, removed " + book.title);
+            }
+        }
+    }
+
+    public void removeFromList(String author, String title, int edition){
+        for (Book book : books){
+            if (book.author.equals(author) && book.title.equals(title) && book.edition == edition){
+                books.remove(book);
+                System.out.println("Alright, removed " + book.title);
+            }
+        }
+    }
+
+    public void sortList(String method){ //methods: title, genre, author, subject
+        //just for giggles, what if we do quicksort
+    }
+
+    public void printTable(){
 
     }
 }
